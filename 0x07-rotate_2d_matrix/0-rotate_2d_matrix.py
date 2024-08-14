ec6a11cbe624af54264e7b0a8f48320d4674602a
mahakamal-e <mahakamal09@gmail.com>
2024-08-14 17:07:03 +0000

    add

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,5 +6,5 @@ def rotate_2d_matrix(matrix):
     """Rotated the given n x x 2d matrix 90 degree"""
     n = len(matrix)
     transposed = zip(*matrix)
-    reverse_rows = [list(row)[::-1] for row in transposed]
+    reversed_rows = [list(row)[::-1] for row in transposed]
     matrix[:] = reversed_rows
